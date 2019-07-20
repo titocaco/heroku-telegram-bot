2b9e12854a166de6a766d6ca29fda39acf65aecf
Tito Caco Curimbaba Spadini <titocaco.jazz@gmail.com>
2019-07-20 19:17:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,6 @@ import telebot
 token = os.environ['TELEGRAM_TOKEN']
 #             ...
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
-
 #       Your bot code below
 # bot = telebot.TeleBot(token)
 # some_api = some_api_lib.connect(some_api_token)
